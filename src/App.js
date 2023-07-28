02ce5545039b740165ea57fdace068bd35f807da
eren ozcan <erenozcandev@gmail.com>
2023-07-28 17:23:39 +0300

    update data process end

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
 import Todo from './Todo';
 import {db} from './firebase';
-import { query, collection, onSnapshot } from "firebase/firestore"; 
+import { query, collection, onSnapshot, updateDoc, doc } from "firebase/firestore"; 
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
@@ -31,10 +31,15 @@ function App() {
     return () => unsubscribe();
   }, []);
 
-
-
-
 // Update todo from firebase
+const toggleComplete = async (todo) => {
+  await updateDoc(doc(db, "todos", todo.id), {
+    completed: !todo.completed
+  });
+};
+
+
+
 // Delete todo
 
 
@@ -55,7 +60,7 @@ function App() {
         </form>
         <ul>
           {todos.map((todo, index) => (
-            <Todo key={index} todo={todo} />
+            <Todo key={index} todo={todo} toggleComplete={toggleComplete} />
           ))}
         </ul>
         <p className={style.count}>You have 2 todos!</p>
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -11,12 +11,12 @@ const style = {
   button: `cursor-pointer flex items-center`
 }
 
-const Todo = ({todo}) => {
+const Todo = ({ todo, toggleComplete }) => {
   return (
     <li className={todo.completed ? style.liComplete : style.li}>
       <div className={style.row}>
-        <input type="checkbox" checked={todo.completed ? 'checked' : ''} />
-        <p className={todo.completed ?  style.textComplete : style.text}>
+        <input onChange={() => toggleComplete(todo)} type="checkbox" checked={todo.completed ? 'checked' : ''} />
+        <p onClick={() => toggleComplete(todo)} className={todo.completed ?  style.textComplete : style.text}>
           {todo.text}  
         </p>  
       </div>
