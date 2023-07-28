b6dfbd1db2861ebc92ba81203b5217246cf700d5
eren ozcan <erenozcandev@gmail.com>
2023-07-28 17:43:02 +0300

    delete todo process end

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
 import Todo from './Todo';
 import {db} from './firebase';
-import { query, collection, onSnapshot, updateDoc, doc, addDoc } from "firebase/firestore"; 
+import { query, collection, onSnapshot, updateDoc, doc, addDoc, deleteDoc } from "firebase/firestore"; 
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
@@ -53,6 +53,9 @@ function App() {
   };
 
 // Delete todo
+  const deleteTodo = async (id) => {
+    await deleteDoc(doc(db, "todos", id));
+  };
 
 
   return (
@@ -65,7 +68,7 @@ function App() {
         </form>
         <ul>
           {todos.map((todo, index) => (
-            <Todo key={index} todo={todo} toggleComplete={toggleComplete} />
+            <Todo key={index} todo={todo} toggleComplete={toggleComplete} deleteTodo={deleteTodo} />
           ))}
         </ul>
 
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -11,7 +11,7 @@ const style = {
   button: `cursor-pointer flex items-center`
 }
 
-const Todo = ({ todo, toggleComplete }) => {
+const Todo = ({ todo, toggleComplete, deleteTodo }) => {
   return (
     <li className={todo.completed ? style.liComplete : style.li}>
       <div className={style.row}>
@@ -20,7 +20,7 @@ const Todo = ({ todo, toggleComplete }) => {
           {todo.text}  
         </p>  
       </div>
-      <button>{ <FaRegTrashAlt /> }</button>   
+      <button onClick={() => deleteTodo(todo.id)}>{<FaRegTrashAlt />}</button>   
     </li>
   )
 }
